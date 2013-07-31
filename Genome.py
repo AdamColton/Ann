68928481013e1934cdfc6e54542aafe8a21817e3
Adam Colton <adamcolton@gmail.com>
2013-07-30 20:02:47 -0400

    Expanding Genome

--- Genome.py
+++ Genome.py
@@ -1,4 +1,5 @@
 import Ann
+import random
 
 # geneome is an Ann constructor. It has genes and a set of fragments the genes instruct how to composite the fragments into a neural net
 class Genome:
@@ -10,7 +11,14 @@ class Genome:
 # the input and output neurons of a fragment do not need to be the input and output neurons of a neural net, they can be a subset
 # or fragments can be stacked with the output of one serving as the input for the next.
 class Fragment:
-  def __init__(self, inputs, outputs, hidden):
-    self.inputs = inputs
-    self.outputs = outputs
-    self.hidden = hidden
+  def __init__(self, inputs, outputs, hidden, density):
+    self.inputs = [Ann.Neuron() for i in range(inputs)]
+    self.outputs = [Ann.Neuron() for i in range(outputs)]
+    self.hidden = [Ann.Neuron() for i in range(hidden)]
+    for hidden in self.hidden:
+      for input in self.inputs:
+        if random.random() < density:
+          hidden.addSynapse(input, random.random())
+      for ouput in self.outputs:
+        if random.random() < density:
+          ouput.addSynapse(hidden, random.random())
