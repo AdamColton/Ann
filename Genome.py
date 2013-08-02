f4f983d81329ba246f9e021e4a9b611306ba1a41
Adam Colton <adamcolton@gmail.com>
2013-08-01 22:49:11 -0400

    Initial Scaffolding

--- Genome.py
+++ Genome.py
@@ -38,21 +38,36 @@ class PatternNeuron:
     self.synapses = []
     self.val = val
     self.bias = bias
+    self.id = str(random.randint(0,2**30))
   def addSynapse(self, neuron, weight):
     self.synapses.append( (neuron, weight) )
     
 class Scaffold:
   def __init__(self, inputs, outputs, patterns, network):
     self.mapping = {}
+    #if there's more neural inputs than pattern[0] inputs -> ignore extra neural inputs
+    #if there's more pattern[0] inputs, than neural inputs, multiple pattern[0] inputs will map to one neural inputs
+    numberOfInputs = min( len(inputs), len(patterns[0].inputs) )
+    self.inputs = [inputs[i] for i in range(numberOfInputs)]
+    for i in range(len(pattern[0].inputs)):
+      self.mapping['0_' + patterns[0].inputs[i].id] = self.inputs[i%numberOfInputs]
     
-    self.inputs = []
-    for neuron in patterns[0].inputs:
-      scaffoldNeuron = ScaffoldNeuron()
-      self.inputs.append(scaffoldNeuron)
-      self.mapping[neuron] = scaffoldNeuron
-    for pattern in patterns[1:]:
+    numberOfOutputs = min( len(outputs), len(patterns[-1].outputs) )
+    self.outputs = [outputs[i] for i in range(numberOfOutputs)]
+    for i in range(len(patterns[-1].outputs)):
+      self.mapping[str(len(patterns)-1) + '_' + patterns[-1].outputs[i].id] = self.outputs[i%numberOfOutputs]
       
-
-class ScaffoldNeuron:
-  def __init__(self, neuralNode = Null):
-    self.neuralNode = neuralNode
+  def _mapHiddenNodes(self, patterns, network):
+    for i in range(len(patterns)):
+      for hidden in patterns[i].hidden:
+        self.mapping[str(i) + "_" + hidden.id] = network.addNeuron()
+  def _mapIOnodes(self, patterns, network):
+    for i in range(len(patterns)-1):
+      outputs = patterns[i].outputs
+      inputs = pattern[i+1].inputs
+      numberOfNeurons = min(outputs, inputs)
+      neurons = [network.addNeuron() for i in range(numberOfNeurons)]
+      for j in range(len(outputs)):
+        self.mapping[str(i) + "_" + outputs[j].id] = neurons[j%numberOfNeurons]
+      for j in range(len(inputs)):
+        self.mapping[str(i+1) + "_" + inputs[j].id] = neurons[j%numberOfNeurons]
